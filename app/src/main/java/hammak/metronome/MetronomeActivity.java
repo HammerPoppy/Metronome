9bdf78ea9276a06ce8b6cd936ad5782cbd2e0599
Saint Jakuzi <saint3543jakuzi@gmail.com>
2016-08-14 04:26:54 +0300

    Start a project

---
+++ app/src/main/java/hammak/metronome/MetronomeActivity.java
@@ -0,0 +1,15 @@
+package hammak.metronome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MetronomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metronome);
+        // TODO: 14.08.2016 New tread for sounds and delays
+
+    }
+}
