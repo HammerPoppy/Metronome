075dfebe5bc83c0125abb8f85c7c39ae9deef51f
Saint Jakuzi <saint3543jakuzi@gmail.com>
2016-08-15 19:14:16 +0300

    Reformated recieving data from a user

--- app/src/main/java/hammak/metronome/MainActivity.java
+++ app/src/main/java/hammak/metronome/MainActivity.java
@@ -1,49 +1,55 @@
 package hammak.metronome;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import hammak.metronome.SetPeriodDialog.OnCompleteListener;
 
-    TextView tvPeriod;
-    NumberPicker npPeriod;
-    Button bStartMetronome;
-    PeriodAlertDialog dfPeriodAlert;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        OnCompleteListener {
 
-    int periodSec;
+    TextView tvPeriodText, tvPeriodValue;
+    Button bStartMetronome, bSetPeriod;
+    SetPeriodDialog dfPeriodAlert;
+
+    float periodSec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvPeriod = (TextView) findViewById(R.id.tvPeriod);
+        tvPeriodText = (TextView) findViewById(R.id.tvPeriodText);
+        tvPeriodValue = (TextView) findViewById(R.id.tvPeriodValue);
+
         bStartMetronome = (Button) findViewById(R.id.bStartMetronome);
-
-        npPeriod = (NumberPicker) findViewById(R.id.npPeriod);
-
-        //Добавлление от студии
-        assert npPeriod != null;
-
-        npPeriod.setMinValue(1);
-        npPeriod.setMaxValue(600);
-
-
-        //Добавление от студии
-        assert bStartMetronome != null;
+        bSetPeriod = (Button) findViewById(R.id.bSetPeriod);
 
         bStartMetronome.setOnClickListener(this);
+        bSetPeriod.setOnClickListener(this);
     }
+
     @Override
-    public void onClick(View view) {
-        Intent toMetronomeActivity = new Intent(this, MetronomeActivity.class);
-        periodSec = npPeriod.getValue();
-        toMetronomeActivity.putExtra("periodSec", periodSec);
-        startActivity(toMetronomeActivity);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bStartMetronome:
+                Intent toMetronomeActivity = new Intent(this, MetronomeActivity.class);
+                toMetronomeActivity.putExtra("periodSec", periodSec);
+                startActivity(toMetronomeActivity);
+                break;
+            case R.id.bSetPeriod:
+                DialogFragment setPeriodDialog = new SetPeriodDialog();
+                setPeriodDialog.show(getFragmentManager(), setPeriodDialog.getClass().getName());
+        }
+    }
+
+    @Override
+    public void onComplete(Float periodSec) {
+        tvPeriodValue.setText(Float.toString(periodSec));
     }
 }
--- app/src/main/java/hammak/metronome/MetronomeActivity.java
+++ app/src/main/java/hammak/metronome/MetronomeActivity.java
@@ -1,9 +1,12 @@
 package hammak.metronome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MetronomeActivity extends AppCompatActivity {
+
+    int periodSec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,8 @@ public class MetronomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_metronome);
         // TODO: 14.08.2016 New tread for sounds and delays
 
+        Intent fromMainActivity = getIntent();
+
+        periodSec = fromMainActivity.getIntExtra("periodSec", 2);
     }
 }
